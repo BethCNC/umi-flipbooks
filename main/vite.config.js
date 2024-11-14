7e66d544b24b50eab63293172cd4567ba872bbc6
BethCNC <beth@7starsdesign.net>
2024-11-14 17:03:23 -0500

    add Navbar and hero

--- main/vite.config.js
+++ main/vite.config.js
@@ -7,12 +7,21 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
-  assetsInclude: ['**/*.pdf', '**/*.png', '**/*.jpg', '**/*.gif'],
+  assetsInclude: ['**/*.pdf', '**/*.png', '**/*.jpg', '**/*.gif', '**/*.svg', '**/*.json', '**/*.mp4'],
   base: '/',
   server: {
     host: true,
     port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
   },
 });
